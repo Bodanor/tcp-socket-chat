dd29d8cc947ef79e05d810540fab89b7929e8684
bodanor <christos.papadopoulos@student.hepl.be>
2022-08-26 00:43:12 +0200

    Major rework with linked lists to improve stability !

--- spam.py
+++ spam.py
@@ -2,12 +2,13 @@ from ast import arg
 from concurrent.futures import thread
 import socket
 import threading
+from time import sleep
 
 def spam(socket):
     while 1:
         socket.send(b"Spamming")
 
-for i in range (0, 2):
+for i in range (0, 100):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect(("127.0.0.1", 9999))
     x = threading.Thread(target=spam, args=(s,))
